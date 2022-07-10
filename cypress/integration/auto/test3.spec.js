d356121a38bf60598924ac8400b49e2e300e98b0
AbdiMD <50539805+AbdiMD@users.noreply.github.com>
2022-07-10 23:43:47 +0800

    Update on create spec for Jury organization

--- cypress/integration/auto/test3.spec.js
+++ cypress/integration/auto/test3.spec.js
@@ -2,7 +2,7 @@ describe('Test suite 1', function(){
 
     before(function(){
         // runs once before all tests in the block
-        cy.fixture('example').then(function(data){
+        cy.fixture('juryorganization').then(function(data){
             this.data=data
         })
     })
@@ -11,18 +11,19 @@ describe('Test suite 1', function(){
     
     it('login', function(){
         cy.login(this.data.email, this.data.password)
-        cy.contains(this.data.menuToClick).click()
-        cy.get('mat-list-item a').each(($el, index, $list) => {
+        cy.get('mat-nav-list > mat-list-item > .mat-list-item-content > a > span').each(($el, index, $list) => {
             if ($el.text() === 'Certification'){
                 cy.wrap($el).click()
-                cy.get('mat-list-item a').each(($el, index, $list) => {
+                cy.get('mat-nav-list > mat-list-item > .mat-list-item-content > mat-nav-list > mat-list-item').each(($el, index, $list) => {
                     if ($el.text() === 'Jury Organization'){
                         cy.wrap($el).click()
-                        cy.get('navbar-brand').should('have.value','Jury Organization')
+                        // cy.get('navbar-brand').contains(this.data.navbarBrand)
                     }
                 })
             }
         })
+        cy.get('.cdk-column-titleFilter > .mat-form-field > .mat-form-field-wrapper > .mat-form-field-flex > .mat-form-field-infix').type('S-CPEB')
+        cy.get(':nth-child(1) > .icon-hand > a').click()
         
         
     })
